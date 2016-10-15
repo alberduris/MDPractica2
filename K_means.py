c298ad3dd005644585f9b9cd0a69847ed62aa11f
Alber <4lb3rduris@gmail.com>
2016-10-15 13:56:29 +0200

    Resubir

---
+++ K_means.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@authors: Julen, Alberto, Till
+
+"""
+
+class K_means:
+
+	def __init__(self, numClusters, opcIni, distMink, distInt, crit, cte ): #instances,):
+		print "trabajo por hacer D:"
+
+
+
+
